5e4db412886aed9297b01f561ecfb09f1ccc481e
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-08-30 22:09:49 -0700

    add: add graphics

---
+++ main.py
@@ -0,0 +1,11 @@
+from graphics import *
+
+
+def main():
+    win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_for_close()
+
+
+main()
